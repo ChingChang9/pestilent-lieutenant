e782eb93e38f7d4db54c36c200cdcc88d2f4bb04
ChingChang9 <chingtheprogrammer@gmail.com>
2021-01-31 16:37:19 -0500

    Fetch lastUrl on start - Using top-level async/await

--- index.js
+++ index.js
@@ -8,32 +8,35 @@ redditAuth = null;
 Snoowrap = null;
 delete require.cache[require.resolve("snoowrap")];
 
-let lastUrl;
+(async () => {
+	let lastUrl = await user.getSavedContent({limit: 1})[0].url;
 
-setInterval(async () => {
-	const newSaved = await user.getSavedContent({limit: 1})[0];
-	if (lastUrl && lastUrl !== newSaved.url) {
-    const client = https.request(`https://discord.com/api/webhooks/${ discordWebhook.id }/${ discordWebhook.token }`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
+	setInterval(async () => {
+		const newSaved = await user.getSavedContent({limit: 1})[0];
+		if (lastUrl !== newSaved.url) {
+			const client = https.request(`https://discord.com/api/webhooks/${ discordWebhook.id }/${ discordWebhook.token }`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				}
+			});
 
-    client.write(JSON.stringify({
-      embeds: [{
-        color: 16711422,
-        author: {
-          name: newSaved.title.slice(0, 256),
-          url: `https://www.reddit.com${ newSaved.permalink }`
-        },
-        image: {
-          url: newSaved.url
-        }
-      }]
-    }));
-    client.end();
-    client.on("error", error => reject(error));
-	}
-	lastUrl = newSaved.url;
-}, 30 * 1000);
+			client.write(JSON.stringify({
+				embeds: [{
+					color: 16711422,
+					author: {
+						name: newSaved.title.slice(0, 256),
+						url: `https://www.reddit.com${ newSaved.permalink }`
+					},
+					image: {
+						url: newSaved.url
+					}
+				}]
+			}));
+			client.end();
+			client.on("error", error => reject(error));
+
+			lastUrl = newSaved.url;
+		}
+	}, 10 * 1000);
+})();
